f6e71cad4ebf2ad5fba5c115bff937c5bba49715
Herbert Duerr <hdu@apache.org>
2022-08-08 19:51:35 +0200

    add test for draining dataset with a While-op loop
    Draining a dataset with a While-op instead of a hasOptional-fetch loop
    as in TestDrainDataset() almost doubles the performance of tight loops:
    40us vs 75us

--- tensorflow/go/op/op_func_test.go
+++ tensorflow/go/op/op_func_test.go
@@ -20,11 +20,12 @@ import (
 	"math"
 	"math/rand"
 	"testing"
+	"time"
 
 	tf "github.com/hdu-hh/tensorflow/tensorflow/go"
 )
 
-func TestFuncCall1(t *testing.T) {
+func TestFuncCall(t *testing.T) {
 	mkFn1 := func(s *Scope, x ...tf.Output) (y []tf.Output, outNames []string, desc string) {
 		y1 := Add(s, x[0], x[1])
 		y2 := Pow(s, x[0], x[1])
@@ -48,6 +49,7 @@ func TestFuncCall1(t *testing.T) {
 		t.Errorf("bad power result: got %d, want %d", got, want)
 	}
 }
+
 func TestForOperation(t *testing.T) {
 	mkBodyFn := func(s *Scope, x ...tf.Output) (outs []tf.Output, outNames []string, desc string) {
 		y1 := Cast(s, x[0], tf.Double)
@@ -86,7 +88,7 @@ func TestForOperation(t *testing.T) {
 	}
 }
 
-func TestWhile1(t *testing.T) {
+func TestWhileClassicLoop(t *testing.T) {
 	// get function for While condition
 	mkCondFn := func(s *Scope, x ...tf.Output) (outs []tf.Output, outNames []string, desc string) {
 		limit, value := x[0], x[1]
@@ -143,5 +145,75 @@ func TestWhile1(t *testing.T) {
 	gotCounter := fetched[2].Value().(int64)
 	if want := int64(463); want != gotCounter {
 		t.Errorf("expected %d iterations, got %v", want, gotCounter)
+	}
+}
+
+// use While operation to drain a dataset
+func TestWhileToDrainDataset(t *testing.T) {
+	// define dataset contents
+	var (
+		types     = []tf.DataType{tf.Int64}
+		shapes    = []tf.Shape{tf.ScalarShape()}
+		loopLimit = 1000
+	)
+
+	// x[0] is the iterator
+	// x[1] is the optional value from the iterator
+	mkCondFn := func(s *Scope, x ...tf.Output) (outs []tf.Output, outNames []string, desc string) {
+		y := OptionalHasValue(s, x[1])
+		return []tf.Output{y}, nil, "return if the optional has data"
+	}
+
+	// x[0] is the iterator
+	// x[1] is the optional from the iterator
+	// x[2] is the sum before optional's value is added
+	mkBodyFn := func(s *Scope, x ...tf.Output) (outs []tf.Output, outNames []string, desc string) {
+		// use optional's value
+		val := OptionalGetValue(s, x[1], types, shapes)[0]
+		sum := Add(s, x[2], val)
+		// prepare next iteration
+		opt := IteratorGetNextAsOptional(s, x[0], types, shapes)
+		return []tf.Output{x[0], opt, sum}, nil, "add up iterated values"
+	}
+	condFn, bodyFn := BuildFuncPair("condFn", "bodyFn", mkCondFn, mkBodyFn, tf.Resource, tf.Variant, tf.Int64)
+
+	var (
+		s = NewScope()
+		// simple RangeDataset for this test
+		cOne    = Const(s, int64(1))
+		cLimit  = Const(s, int64(loopLimit))
+		dataset = RangeDataset(s, cOne, cLimit, cOne, types, shapes)
+		dsIter  = Iterator(s, "", "", types, shapes)
+		mkIter  = MakeIterator(s, dataset, dsIter)
+		// prepare the initial inputs of the while loop
+		optInit = IteratorGetNextAsOptional(s, dsIter, types, shapes)
+		sumInit = Const(s, int64(0))
+		// add the While operation to the graph
+		_     = s.RegisterFunc(condFn, nil)
+		_     = s.RegisterFunc(bodyFn, nil)
+		while = While(s, []tf.Output{dsIter, optInit, sumInit}, condFn, bodyFn)
+	)
+	graph, err := s.Finalize()
+	if err != nil {
+		t.Fatal(err)
+	}
+	sess, err := tf.NewSession(graph, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// connect the iterator to the dataset
+	if _, err := sess.Run(nil, nil, []*tf.Operation{mkIter}); err != nil {
+		t.Fatal(err)
+	}
+	// run the while loop
+	startTime := time.Now()
+	fetched, err := sess.Run(nil, []tf.Output{while[2]}, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	dt := float64(time.Since(startTime).Nanoseconds())
+	t.Logf("%d loops took %.1fms -> %.1fus/loop", loopLimit, dt*1e-6, dt*1e-3/float64(loopLimit))
+	if got, want := fetched[0].Value().(int64), int64((loopLimit-1)*loopLimit/2); want != got {
+		t.Errorf("sum of while loop wrong: got %d, want %d", got, want)
 	}
 }
