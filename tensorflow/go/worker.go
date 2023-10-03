d3f5101740398ffdba12766ff9d56a7b8ae2ed9b
Herbert Duerr <hdu@apache.org>
2023-10-03 08:19:47 +0200

    add CastTensor()
    using a new worker session that could also be useful for other needs

---
+++ tensorflow/go/worker.go
@@ -0,0 +1,62 @@
+// workerGraph is used e.g. for converting tensors
+package tensorflow
+
+import "fmt"
+
+type (
+	dtypePair struct{ inp, out DataType }
+	opPair    struct{ inp, out *Operation }
+	workerObj struct {
+		graph   *Graph
+		session *Session
+		castMap map[dtypePair]opPair
+	}
+)
+
+var worker workerObj
+
+func CastTensor(dtype DataType, inp *Tensor) (out *Tensor, err error) {
+	// short circuit if no casting is needed
+	if inp.DataType() == dtype {
+		return inp, nil
+	}
+
+	// ensure the worker session
+	if worker.session == nil {
+		worker.graph = NewGraph()
+		worker.castMap = map[dtypePair]opPair{}
+	}
+
+	// get operations needed for type casting
+	castKey := dtypePair{inp.DataType(), dtype}
+	castOps, ok := worker.castMap[castKey]
+	if !ok {
+		castOps.inp, err = worker.graph.AddOperation(OpSpec{Type: "Const",
+			Name:  fmt.Sprintf("castInp_%d_%d", castKey.inp, castKey.out),
+			Attrs: map[string]any{"value": inp, "dtype": inp.DataType()},
+		})
+		if err != nil {
+			return
+		}
+		castOps.out, err = worker.graph.AddOperation(OpSpec{Type: "Cast",
+			Name:  fmt.Sprintf("castOut_%d_%d", castKey.inp, castKey.out),
+			Input: []Input{castOps.inp.Output(0)},
+			Attrs: map[string]interface{}{"DstT": dtype},
+		})
+		if err != nil {
+			return
+		}
+		worker.castMap[castKey] = castOps
+		worker.session, err = NewSession(worker.graph, nil)
+		if err != nil {
+			return
+		}
+	}
+
+	// use the worker session to convert the tensor
+	fetches, err := worker.session.Run(FeedMap{castOps.inp.Output(0): inp}, castOps.out.Outputs(), nil)
+	if err != nil {
+		return
+	}
+	return fetches[0], nil
+}
---
+++ tensorflow/go/worker_test.go
@@ -0,0 +1,53 @@
+package tensorflow
+
+import (
+	"math"
+	"testing"
+)
+
+func TestCastWorker(t *testing.T) {
+	const V = 123.456
+	for _, sample := range []struct {
+		name  string
+		value float64
+		dtype DataType
+	}{
+		{"bool", 1, Bool},
+		{"int8", 123, Int8},
+		{"int16", 123, Int16},
+		{"int32", 123, Int32},
+		{"int64", 123, Int64},
+		{"uint8", 123, Uint8},
+		{"uint16", 123, Uint16},
+		{"uint32", 123, Uint32},
+		{"uint64", 123, Uint64},
+		{"qint8", 123, Qint8},
+		{"qint16", 123, Qint16},
+		{"uint32", 123, Qint32},
+		{"quint8", 123, Quint8},
+		{"quint16", 123, Quint16},
+		{"float", 123.456, Float},
+		{"double", 123.456, Double},
+		{"bfloat16", 123.5, Bfloat16},
+		{"half", 123.5, Half},
+		{"f8_e5m2", 128, Float8e5m2},
+		{"f8_e4m3", 120, Float8e4m3fn},
+	} {
+		t.Run(sample.name, func(t *testing.T) {
+			x1, err := NewTypedTensor(sample.dtype, V)
+			if err != nil {
+				t.Fatal(err)
+			}
+			x2, err := CastTensor(Double, x1)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if got := x1.DataType(); got != sample.dtype {
+				t.Errorf("wrong dtype: got %T, want %s", got, sample.name)
+			}
+			if got := x2.Value().(float64); math.Abs(got-sample.value) > 0.1 {
+				t.Errorf("wrong value: got %v, want %v", got, V)
+			}
+		})
+	}
+}
